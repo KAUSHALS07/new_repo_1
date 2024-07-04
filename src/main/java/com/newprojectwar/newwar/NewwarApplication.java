5aacc918ce3efef729f80ac2eb8de0e86524293d
ASUS <kaushalsuwar1019@gmail.com>
2024-07-04 20:48:31 +0530

    1st commit create controller

---
+++ src/main/java/com/newprojectwar/newwar/NewwarApplication.java
@@ -0,0 +1,13 @@
+package com.newprojectwar.newwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewwarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewwarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newprojectwar/newwar/NewwarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newprojectwar.newwar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewwarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
