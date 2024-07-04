f1c962955dc0249405a058314a3a175d22a49e5d
ASUS <kaushalsuwar1019@gmail.com>
2024-07-04 20:56:06 +0530

    1st method

--- src/main/java/com/newprojectwar/newwar/controller/UserController.java
+++ src/main/java/com/newprojectwar/newwar/controller/UserController.java
@@ -3,7 +3,13 @@ package com.newprojectwar.newwar.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
+@RequestMapping("/apiwar")
 public class UserController {
+
+    @RequestMapping("/user")
+    public String getUser() {
+        return "this is war project";
+    }
 
 }
